c53aed9979984a14cbc89d0db2ee6d81249b7275
youvasri324 <youvasri324@gmail.com>
2023-04-16 12:57:45 +0530

    space added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
         let slideIndex = 0;
+        
         showSlides();
 
         function showSlides() {
+
             let i;
             let slides = document.getElementsByClassName("mySlides");
             let dots = document.getElementsByClassName("dot");
